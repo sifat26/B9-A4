d6c9167d144b72590c93fc0e15f292e5e9a5cd65
sifat26 <sifatict26@gmail.com>
2024-02-08 01:41:24 +0600

    4 Complete

--- Make A Great Password.js
+++ Make A Great Password.js
@@ -1,4 +1,25 @@
 
 function password(obj) {
-    
+  const name = obj.name;
+  const birthYear = obj.birthYear;
+  const siteName = obj.siteName;
+
+  if (
+    obj.hasOwnProperty("name") &&
+    obj.hasOwnProperty("birthYear") &&
+    obj.hasOwnProperty("siteName")
+  ) {
+    if (birthYear.toString().length < 4) {
+        return "invalid";
+      
+    } else {
+      let s=siteName.charAt(0).toUpperCase() + siteName.slice(1);
+        let pass = s + "#" + name + "@" + birthYear;
+      return pass;
+      
     }
+  } else {
+    return "invalid";
+  }
+}
+console.log(password({ name: "toky", birthYear: 2000, siteName: "facebook" }));
